7a3f24b87c52a31cd00975ac5c6a010b3ea30bbb
kellyvasquez <kefervaza.19@hotmail.com>
2017-02-15 16:55:34 -0500

    Menu while terminado

--- taller_1_while.cpp
+++ taller_1_while.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
-#include <windows.h>
 #include <conio.h>
+#include <stdlib.h>
 using namespace std;
-
-void gotoxy(int x,int y)
- {
-    HANDLE hcon;
-    hcon = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD dwPos;
-    dwPos.X = x;
-    dwPos.Y= y;
-    SetConsoleCursorPosition(hcon,dwPos);
- }
 
 int main ()
 {	int opc;
 
-			do {gotoxy(37,2); cout<<"MENU WHILE";
-  				gotoxy(20,5); cout<<"Elija una opcion: ";
-  				gotoxy(20,8); cout<<"1. Imprimir en pantalla los numeros del 1 al 100.";
-  				gotoxy(20,10); cout<<"2. Sumar los numeros del 1 al 100.";
-  				gotoxy(20,12); cout<<"3. Sumar los numeros pares del 1 al 50.";
- 			    gotoxy(20,14); cout<<"4. Sumar los numeros impares del 1 al 50";
-                gotoxy(20,16); cout<<"0. SALIR "; cin>>opc;
+			do {system("cls");
+                cout<<"MENU WHILE\n";
+  				cout<<endl<<endl;
+  				cout<<"1. Imprimir en pantalla los numeros del 1 al 100.\n";
+  				cout<<"2. Sumar los numeros del 1 al 100.\n";
+  				cout<<"3. Sumar los numeros pares del 1 al 50.\n";
+ 			    cout<<"4. Sumar los numeros impares del 1 al 50\n";
+                cout<<"0. SALIR ";
+                cout<<endl<<endl;
+                cout<<"\nElija una opcion: ";cin>>opc;
 
 
 
@@ -31,24 +24,68 @@ int main ()
             opc=0;
             break;
         case 1:
+            system ("cls");
+            cout<<"NUMEROS DEL 1 AL 100.\n";
             int num;
             num=1;
             while (num<=100) {
-            cout<< num;cout<<" ";
-            num++; }
+                cout<<"\n"<<num<<" ";
+                num++; }
             getch();
             break;
-
+        case 2:
+            system ("cls");
+            cout<<" LA SUMA DE LOS NUMEROS DEL 1 AL 100.\n";
+            int numero, suma;
+            numero=1;
+            suma=0;
+            while (numero<=100) {
+                suma=numero+suma;
+                numero++;
+                }
+                cout<<"\nLa suma de los numeros del 1 al 100 es: "<<suma;
+            getch;
+            break;
+        case 3:
+            system ("cls");
+            cout<<"SUMA DE LOS NUMEROS PARES DEL 1 AL 50.\n";
+            int par, sum;
+            par=1;
+            sum=0;
+            while (par<50) {
+                if (par%2==0)
+                {
+                    sum=par+sum;
+                }
+                par++;
+                }
+                cout<<"\nLa suma de los pares del 1 al 50 es: "<<sum;
+            getch ();
+            break;
+        case 4:
+            system ("cls");
+            cout<<"SUMA DE LOS NUMEROS IMPARES DEL 1 AL 50.\n";
+            int impar, s;
+            impar=1;
+            s=0;
+            while (impar<50) {
+                if (impar%2==0){
+                 }else{
+                    s=impar+s;
+                }
+                impar++;
+                }
+                cout<<"\nLa suma de los impares del 1 al 50 es: "<<s;
+            getch ();
+            break;
         default:
-            cout<<"El valor ingresado no es valido. Debe ser 1, 2, 3, 4 o 0"<<endl;
+            cout<<"\nEl valor ingresado no es valido. Debe ser 1, 2, 3, 4 o 0"<<endl;
 				}
 				getch();
 			}
 				while (opc>0);
 				cout<<endl<<endl;
 
-				system("pause");
-
-
+				system ("pause");
 	return 0;
 }
